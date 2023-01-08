e53d257e40c2d15e4a9c14f3e218e229dd2801a2
Michael Kiros <michael.kiros00@gmail.com>
2023-01-08 18:18:45 -0500

    commit tree 7b49c50ea9d288b950df1a40ba2c4404f584092a

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("The DOM has loaded");
+    let text = document.getElementById('text')
+    text.textContent = 'This is really cool!'
+  });
